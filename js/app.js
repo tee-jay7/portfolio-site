934c3822b0b759734e3ed5a6e03e52964c046189
tee-jay7 <twasam.john@gmail.com>
2024-07-19 12:16:59 +0000

    finalized changes

---
+++ js/app.js
@@ -0,0 +1,44 @@
+const getElement = (selector) => {
+  const element = document.querySelector(selector)
+
+  if (element) return element
+  throw Error(
+    `Please double check your class names, there is no ${selector} class`
+  )
+}
+
+const getViewsfromDb = async () => {
+  try {
+    const results = await fetch('https://1ockk7b00c.execute-api.eu-west-1.amazonaws.com/Prod/visits');
+    
+    if (!results.ok) {
+      throw new Error(`HTTP error! status: ${results.status}`);
+    }
+    
+    const data = await results.json();
+    const { _, updated_value } = data;
+    views.textContent = `This site has been visited ${updated_value} times 🚀`;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    views.textContent = 'Error fetching view count 😓⛑️';
+  }
+};
+
+
+
+const links = getElement('.nav-links')
+const navBtnDOM = getElement('.nav-btn')
+
+navBtnDOM.addEventListener('click', () => {
+  links.classList.toggle('show-links')
+})
+
+const date = getElement('#date')
+const currentYear = new Date().getFullYear()
+date.textContent = currentYear
+
+
+
+
+const views = getElement('#views')
+getViewsfromDb()
